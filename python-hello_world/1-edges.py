b639681a4f224b84f0b1228c337c75d12d1f02a0
siedd <saidmwacharo8@gmail.com>
2023-11-30 15:08:21 +0300

    words

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -2,6 +2,6 @@ word = "Holberton"
 word_first_3 = word[:3]
 middle_word = word[1:-1]
 word_last_2 = word[-2:]
-print("First three letters:",word_first_3)
-print("Last two letters:",word_last_2)
+print("First 3 letters:",word_first_3)
+print("Last 2 letters:",word_last_2)
 print("Middle word:",middle_word)   
