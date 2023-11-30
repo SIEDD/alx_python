31a2fb313e01cc8041999cf0a09a7532ea4412ba
siedd <saidmwacharo8@gmail.com>
2023-11-30 14:26:42 +0300

    yay python

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,1 +1,16 @@
+import random
+
+number = random.randint(-10, 10)
+
+print(number)
+
+if number > 0:
+    print("is positive")
+elif number == 0:
+    print("is zero")
+elif number < 0:
+    print("is negative")
+else:
+    print("enter another number\n")
+
 
--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,1 +1,4 @@
+for i in range(99):
+    print("{:d} = 0x{:x}".format(i, i))
+
 
