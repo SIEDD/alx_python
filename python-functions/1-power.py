c40fe8bbe9175b224da4434fee4ce8d207ca37ee
siedd <saidmwacharo8@gmail.com>
2023-11-30 16:33:37 +0300

    pow

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,7 +1,5 @@
 def pow(a, b):
-    return a^b
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+    result = 1
+    for _ in range(b):
+        result *= a
+    return result
