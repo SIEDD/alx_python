06a37fc9e52ef80d062885abab0d0c5673339b20
siedd <saidmwacharo8@gmail.com>
2023-12-07 22:16:09 +0300

    div

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -13,6 +13,20 @@
 # safe_print_division(10, 2)
 
 
+# def safe_print_division(a, b):
+#     try:
+#         result = a / b
+#     except ZeroDivisionError:
+#         print("Inside result: {}".format(None))
+#         return None
+#     else:
+#         print("Inside result: {}".format(result))
+#         return result
+#     finally:
+#         print("{a} / {b} = {}".format(result, a=a, b=b))
+
+# safe_print_division(10, 2)
+
 def safe_print_division(a, b):
     try:
         result = a / b
@@ -23,6 +37,9 @@ def safe_print_division(a, b):
         print("Inside result: {}".format(result))
         return result
     finally:
-        print("{a} / {b} = {}".format(result, a=a, b=b))
+        if 'result' in locals():
+            print("{a} / {b} = {}".format(result, a=a, b=b))
+        else:
+            print("{a} / {b} = {}".format(None, a=a, b=b))
 
 safe_print_division(10, 2)
