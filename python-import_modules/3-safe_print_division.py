1a0272ba71d0485687fa6ae3fc9a5c8135fba36b
siedd <saidmwacharo8@gmail.com>
2023-12-07 22:12:41 +0300

    division

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,15 +1,28 @@
+# def safe_print_division(a, b):
+#     #try:
+#         result = a / b
+#     #except ZeroDivisionError:
+#         #print("Inside result: {}".format(None))
+#         #return None
+#    # else:
+#         print("Inside result: {}".format(result))
+#         return None
+#     #finally:
+#      #   print("Inside result: {}".format(None))
+
+# safe_print_division(10, 2)
+
+
 def safe_print_division(a, b):
-    #try:
+    try:
         result = a / b
-    #except ZeroDivisionError:
-        #print("Inside result: {}".format(None))
-        #return None
-   # else:
-        print("Inside result: {}".format(result))
+    except ZeroDivisionError:
+        print("Inside result: {}".format(None))
         return None
-    #finally:
-     #   print("Inside result: {}".format(None))
+    else:
+        print("Inside result: {}".format(result))
+        return result
+    finally:
+        print("{a} / {b} = {}".format(result, a=a, b=b))
 
 safe_print_division(10, 2)
-
-
