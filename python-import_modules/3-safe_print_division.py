e5a54f4918d53e4a3ed7803de73d97dac8ac706c
siedd <saidmwacharo8@gmail.com>
2023-12-07 22:19:11 +0300

    div

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,16 +1,16 @@
-# def safe_print_division(a, b):
-#     #try:
-#         result = a / b
-#     #except ZeroDivisionError:
-#         #print("Inside result: {}".format(None))
-#         #return None
-#    # else:
-#         print("Inside result: {}".format(result))
-#         return None
-#     #finally:
-#      #   print("Inside result: {}".format(None))
+def safe_print_division(a, b):
+    #try:
+        result = a / b
+    #except ZeroDivisionError:
+        #print("Inside result: {}".format(None))
+        #return None
+   # else:
+        print("Inside result: {}".format(result))
+        return None
+    #finally:
+     #   print("Inside result: {}".format(None))
 
-# safe_print_division(10, 2)
+safe_print_division(10, 2)
 
 
 # def safe_print_division(a, b):
@@ -27,19 +27,4 @@
 
 # safe_print_division(10, 2)
 
-def safe_print_division(a, b):
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        print("Inside result: {}".format(None))
-        return None
-    else:
-        print("Inside result: {}".format(result))
-        return result
-    finally:
-        if 'result' in locals():
-            print("{a} / {b} = {}".format(result, a=a, b=b))
-        else:
-            print("{a} / {b} = {}".format(None, a=a, b=b))
 
-safe_print_division(10, 2)
