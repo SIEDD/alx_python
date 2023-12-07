636818a4144b0cdc80d7bd31df61aebd2aa961e5
siedd <saidmwacharo8@gmail.com>
2023-12-07 22:56:18 +0300

    exceptions

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,6 +1,11 @@
-def raise_exception():
-    raise TypeError("This is a type exception")
+#def raise_exception():
+#     raise TypeError("exception")
 
-# Example usage:
-raise_exception()
+# # Example usage:
+# raise_exception()
+try:
+    alist = [1,2,3]
+    print(alist[3])
 
+except IndexError:
+    print("exception has been raised")
