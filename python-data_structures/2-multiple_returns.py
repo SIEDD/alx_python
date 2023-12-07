b3eb369c1c74c1c86be94d8868a99ef646680cc3
siedd <saidmwacharo8@gmail.com>
2023-12-08 00:07:14 +0300

    returns

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,11 +1,23 @@
+# def multiple_returns(sentence):
+#     length = len(sentence)
+#     first_char = sentence[0] if length > 0 else None
+#     return (length, first_char)
+
+# sentence = multiple_returns("Holberton is so cool")
+# print(sentence)  # Output: (13, 'H')
+
+# sentence_empty = multiple_returns("")
+# print(sentence_empty)  # Output: (0, None)
+
 def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0] if length > 0 else None
-    return (length, first_char)
+    result = (length, first_char)
+    return result
 
-sentence = multiple_returns("Holberton is so cool")
-print(sentence)  # Output: (13, 'H')
+# Example usage:
+result = multiple_returns("Hello, World!")
+print("Length: {} - First character: {}".format(result[0], result[1]))
 
-sentence_empty = multiple_returns("")
-print(sentence_empty)  # Output: (0, None)
-
+result_empty = multiple_returns("")
+print("Length: {} - First character: {}".format(result_empty[0], result_empty[1]))
