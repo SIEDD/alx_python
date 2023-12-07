9b09e3d0fba2de41a0f522eeddf99aa833065f4c
siedd <saidmwacharo8@gmail.com>
2023-12-07 23:55:39 +0300

    matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,1 +1,19 @@
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col_index, element in enumerate(row):
+            if col_index == len(row) - 1:
+                # Last element in the row
+                print("{:d}".format(element), end="")
+            else:
+                # Not the last element in the row
+                print("{:d} ".format(element), end="")
+        print()  # Move to the next line after each row
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
 
