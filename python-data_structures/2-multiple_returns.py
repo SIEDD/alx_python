bad024b3dc7d231a466fa30a0a3244eb7ad47de7
siedd <saidmwacharo8@gmail.com>
2023-12-08 00:00:02 +0300

    returns

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,1 +1,11 @@
+def multiple_returns(sentence):
+    length = len(sentence)
+    first_char = sentence[0] if length > 0 else None
+    return (length, first_char)
+
+sentence = multiple_returns("At Holberton school, I learnt C!")
+print(sentence)  # Output: (13, 'H')
+
+sentence_empty = multiple_returns("")
+print(sentence_empty)  # Output: (0, None)
 
