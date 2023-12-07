ceb6e625cda2c6441ff0b9cad23dc1bd7a8a48c6
siedd <saidmwacharo8@gmail.com>
2023-12-07 22:22:29 +0300

    vc

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -6,25 +6,11 @@ def safe_print_division(a, b):
         return None
     else:
         print("Inside result: {}".format(result))
-        return None
+        return result
     finally:
         print("Inside result: {}".format(None))
 
+# Example usage:
 safe_print_division(10, 2)
-
-
-# def safe_print_division(a, b):
-#     try:
-#         result = a / b
-#     except ZeroDivisionError:
-#         print("Inside result: {}".format(None))
-#         return None
-#     else:
-#         print("Inside result: {}".format(result))
-#         return result
-#     finally:
-#         print("{a} / {b} = {}".format(result, a=a, b=b))
-
-# safe_print_division(10, 2)
-
+# safe_print_division(5, 0)
 
