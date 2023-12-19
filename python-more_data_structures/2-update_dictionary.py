76833b163ab3ec168a75db34b6ef22c88b12bb92
siedd <saidmwacharo8@gmail.com>
2023-12-19 22:51:48 +0300

    xx

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,9 +1,27 @@
-def print_sorted_dictionary(my_dict):
-    if my_dict is not None:
-        keys = sorted(my_dict.keys())
-        for key in keys:
-            print("{}: {}".format(key, my_dict[key]))
+# def print_sorted_dictionary(my_dict):
+#     if my_dict is not None:
+#         keys = sorted(my_dict.keys())
+#         for key in keys:
+#             print("{}: {}".format(key, my_dict[key]))
 
-# Example usage
-my_dict = {'a': 'A', 'b': 'b', 'c': 'c', 'd': 'd', 'e': 'e'}
-print_sorted_dictionary(my_dict)
+# # Example usage
+# my_dict = {'a': 'a', 'b': 'b', 'c': 'c', 'd': 'd', 'e': 'e'}
+# print_sorted_dictionary(my_dict, 'a', 'A')
+# print(my_dict)
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+
+my_dict = {'a': 'a', 'b': 'b', 'c': 'c', 'd': 'd', 'e': 'e'}
+
+update_dictionary(my_dict, 'a', 'A')
+update_dictionary(my_dict, 'f', 'f')
+
+for k, v in my_dict.items():
+    print(f"{k}: {v}")
+
+print("xx")
+
+
+
+
