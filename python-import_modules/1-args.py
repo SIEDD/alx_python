1dfc2b79b4ea1423b0d5af8c974082c31a0c190a
siedd <saidmwacharo8@gmail.com>
2023-12-19 23:13:26 +0300

    safe

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,11 +2,7 @@ import sys
 
 def print_arguments():
     num_arguments = len(sys.argv) - 1
-
-    # Print the number of arguments
     print(num_arguments, end=" ")
-
-    # Print "argument" or "arguments" based on the number
     if num_arguments == 1:
         print("argument:",end="")
     elif num_arguments > 1:
@@ -15,7 +11,6 @@ def print_arguments():
         print(".", end="")
         return
 
-    # Print each argument along with its position
     for i, arg in enumerate(sys.argv[1:],start=1):
         print("\n{}: {}".format(i, arg))
 
