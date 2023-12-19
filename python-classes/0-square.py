160bfc439ef728eafa3c98d259e6c46f695e97bd
siedd <saidmwacharo8@gmail.com>
2023-12-20 00:01:17 +0300

    sq

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,8 @@
 class Square:
-    def __init__(self, size):
+    def __init__(self, size=5):
         self.__size = size
+
+if __name__ == "__main__":
+    my_square = Square()
+    print("The size of the square is:", my_square.__size)
 
