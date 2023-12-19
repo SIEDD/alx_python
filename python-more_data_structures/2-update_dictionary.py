70a4c4d9b3724adf83c53d7bd5f803a000ac1a3f
siedd <saidmwacharo8@gmail.com>
2023-12-19 22:21:58 +0300

    dict

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,10 +1,7 @@
 def update_dictionary(a_dictionary, key, value):
-    # Update or add the key-value pair in the dictionary
     a_dictionary[key] = value
 
 my_dict = {'a': 'a', 'b': 'b', 'c': 'c', 'd': 'd', 'e': 'e'}
 
 my_dict['a'] = 'A'
-sentence = my_dict.split()
-for i in my_dict:
-    print(i)
+print(my_dict)
