747ed7eb3076017f2721d1071f17eed07e8e8f94
siedd <saidmwacharo8@gmail.com>
2023-12-19 17:31:45 +0300

    hello

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,12 +1,10 @@
-# script.py
-
 import sys
 
 def print_arguments():
     num_arguments = len(sys.argv) - 1
 
     # Print the number of arguments
-    print("Number of argument(s):", num_arguments, end=" ")
+    print(num_arguments, end=" ")
 
     # Print "argument" or "arguments" based on the number
     if num_arguments == 1:
@@ -23,26 +21,3 @@ def print_arguments():
 
 if __name__ == "__main__":
     print_arguments()
-
-# # my_program.py
-# import sys
-
-# def print_arguments():
-#     # Get the number of arguments
-#     num_arguments = len(sys.argv) - 1  # Exclude the script name
-
-#     # Print the header
-#     if num_arguments == 0:
-#         print("0 arguments.")
-#         print(".")
-#     elif num_arguments == 1:
-#         print("1 argument:")
-#     else:
-#         print("{} arguments:".format(num_arguments))
-
-#     # Print each argument
-#     for i in range(1, num_arguments + 1):
-#         print("{}: {}".format(i, sys.argv[i]))
-
-# if __name__ == "__main__":
-#     print_arguments()
