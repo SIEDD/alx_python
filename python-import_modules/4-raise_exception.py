9cb95302591d03f0787e25e800d7202b8e0be631
siedd <saidmwacharo8@gmail.com>
2023-12-19 18:42:26 +0300

    exception

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,22 +1,9 @@
-# def raise_exception():
-#     raise TypeError("exception has been raised")
-
-# # Example usage:
-# raise_exception()
-
-# def raise_exception():
-#     raise TypeError("exception has been raised")
-
-# try:
-#     raise_exception()
-# except TypeError as e:
-#     print(e)
 def raise_exception():
-    raise TypeError("Exception has been raised")
+    raise TypeError()
 
 try:
     raise_exception()
-except TypeError as e:
-    print(e)
-    # Additional handling can be added if needed
+except TypeError:
+    print("Exception has been raised")
+    
 
