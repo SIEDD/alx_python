17d1760e4c923a1ca4fc277bc4229a18434d2fe1
siedd <saidmwacharo8@gmail.com>
2023-12-19 17:54:56 +0300

    end

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,34 +1,19 @@
 import sys
 
-# def print_arguments():
-#     num_arguments = len(sys.argv) - 1
-
-#     # Print the number of arguments
-#     print(num_arguments, end=" ")
-
-#     # Print "argument" or "arguments" based on the number
-#     if num_arguments == 1:
-#         print("argument:", end="")
-#     elif num_arguments > 1:
-#         print("arguments:", end="")
-#     else:
-#         print(".", end="")
-#         return
-
-#     # Print each argument along with its position
-#     for i, arg in enumerate(sys.argv[1:], start=1):
-#         print("\n{}: {}".format(i, arg))
-
-# if __name__ == "__main__":
-#     print_arguments()
-
-import sys
-
 def print_arguments():
     num_arguments = len(sys.argv) - 1
 
     # Print the number of arguments
-    print("{} argument{}:".format(num_arguments, "s" if num_arguments != 1 else ""), end=" ")
+    print(num_arguments, end=" ")
+
+    # Print "argument" or "arguments" based on the number
+    if num_arguments == 1:
+        print("argument:")
+    elif num_arguments > 1:
+        print("arguments:")
+    else:
+        print(".", end="")
+        return
 
     # Print each argument along with its position
     for i, arg in enumerate(sys.argv[1:], start=1):
@@ -36,4 +21,6 @@ def print_arguments():
 
 if __name__ == "__main__":
     print_arguments()
+
+
 
