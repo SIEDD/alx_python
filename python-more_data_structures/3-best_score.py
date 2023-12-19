80423e3e462e469028cb38c1d4622ba06c820e2e
siedd <saidmwacharo8@gmail.com>
2023-12-19 22:54:42 +0300

    marks

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,1 +1,12 @@
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+
+    max_key = max(a_dictionary, key=a_dictionary.get)
+    return max_key
+
+# Example usage:
+scores = {'Alice': 90, 'Bob': 75, 'Charlie': 88, 'David': 95}
+best = best_score(scores)
+print(f"The student with the best score is: {best}")
 
