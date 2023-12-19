913ce542ec51ae391fca61e0ca9a7efdd781ad0d
siedd <saidmwacharo8@gmail.com>
2023-12-19 23:02:37 +0300

    lines

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,9 +1,2 @@
 def multiply_list_map(my_list=[], number=0):
     return list(map(lambda x: x * number, my_list))
-
-# Example usage:
-original_list = [1, 2, 3, 4]
-multiplier = 2
-result = multiply_list_map(original_list, multiplier)
-print(result)
-
