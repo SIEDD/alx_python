f4def5c2755c38a4e7050eba15b953f65917f43e
siedd <saidmwacharo8@gmail.com>
2023-12-19 19:01:31 +0300

    dict

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -2,14 +2,14 @@ def update_dictionary(a_dictionary, key, value):
     # Update or add the key-value pair in the dictionary
     a_dictionary[key] = value
 
-# Example usage:
-# my_dict = {'name': 'John', 'age': 25, 'city': 'New York'}
+#Example usage:
+my_dict = {'a': 'A', 'b': 'b', 'c': 'c', 'd': 'd', 'e': 'e'}
 
-# # Update existing key
-# update_dictionary(my_dict, 'age', 26)
+# Update existing key
+update_dictionary(my_dict, 'a', 89)
 
-# # Add a new key-value pair
-# update_dictionary(my_dict, 'gender', 'Male')
+# Add a new key-value pair
+update_dictionary(my_dict, 'gender', 'Male')
 
-# print(my_dict)
+print(my_dict)
 
