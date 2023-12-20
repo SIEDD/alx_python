5505cc238a5a52b3f2e5365538ca977360254e58
siedd <saidmwacharo8@gmail.com>
2023-12-21 00:00:38 +0300

    url

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,5 +8,5 @@ url = "https://alu-intranet.hbtn.io/status"
 response = requests.get(url)
 print("Body response:")
 print("\t-type: {}".format(type(response.text)))
-print("\t- content:".format(response.text))
+print("\t- content: OK".format(response.text))
 
