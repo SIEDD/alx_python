d9b2aa719cf29c6d22abdce560ef99d611a056bd
siedd <saidmwacharo8@gmail.com>
2023-12-20 19:55:17 +0300

    geo

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,3 +1,13 @@
+"""Module: base_geometry
+
+This module defines the BaseGeometry class, which represents basic geometry.
+
+Classes:
+    BaseGeometry: A base class for basic geometry operations.
+
+Methods:
+    area(self): Raises an exception indicating that area() is not implemented.
+"""
 class BaseGeometry:
     """A base class representing basic geometry."""
     
