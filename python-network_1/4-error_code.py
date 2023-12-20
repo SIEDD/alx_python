c3646a217ba2e848d6bf3a45c99ad41da35a09a7
siedd <saidmwacharo8@gmail.com>
2023-12-21 01:30:03 +0300

    error

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,1 +1,32 @@
+# import sys
+# import requests
 
+# url = sys.argv[1]
+# if status_code >=400:
+#     print("error code: ")
+import requests
+import sys
+
+def fetch_url(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
+        
+        # If successful, print the response content
+        print(response.text)
+    except requests.exceptions.HTTPError as err:
+        # If there's an HTTP error, print the error code
+        print(f"Error code: {err.response.status_code}")
+    except requests.exceptions.RequestException as e:
+        # For other request errors, print a generic error message
+        print(f"Error: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    # Example usage: python script.py http://example.com
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
+    
+    url = sys.argv[1]
+    fetch_url(url)
