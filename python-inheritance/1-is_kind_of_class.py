7afcfdad430400c739e011b7a699c8938e1fd7b0
siedd <saidmwacharo8@gmail.com>
2023-12-20 18:21:43 +0300

    t/f

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,1 +1,26 @@
+def is_kind_of_class(obj, a_class):
+    """Check if obj is an instance or inherited from a_class."""
+    return isinstance(obj, a_class)
+
+# Example usage:
+class ParentClass:
+    pass
+
+class ChildClass(ParentClass):
+    pass
+
+class AnotherClass:
+    pass
+
+obj1 = ParentClass()
+obj2 = ChildClass()
+obj3 = AnotherClass()
+
+result1 = is_kind_of_class(obj1, ParentClass)
+result2 = is_kind_of_class(obj2, ParentClass)
+result3 = is_kind_of_class(obj3, ParentClass)
+
+print(result1)  # Output: True
+print(result2)  # Output: True
+print(result3)  # Output: False
 
