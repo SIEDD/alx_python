0df36109311daeb4551f48564249a8d2f2678621
siedd <saidmwacharo8@gmail.com>
2023-12-21 00:57:03 +0300

    email

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,1 +1,13 @@
+"""importing system and requests"""
+import sys
+import requests
+"""variables that store url and email"""
+url = input("enter URL: ") if len(sys.argv) < 2 else sys.argv[1]
+email = input("enter email: ") if len(sys.argv) < 3 else sys.argv[2]
 
+data = {
+    'email':email
+}
+"""sending post to the url"""
+response = requests.post(url=url, data=data)
+print(response.text)
