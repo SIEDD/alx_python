c0049ef6055c1d55f477ae6bd272fe57d40e709c
siedd <saidmwacharo8@gmail.com>
2023-12-20 18:00:10 +0300

    in

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -2,13 +2,9 @@ def is_same_class(obj, a_class):
     """Check if obj is an instance of a_class."""
     return type(obj) is a_class
 
-# Example usage:
 class ExampleClass:
     pass
 
 obj = ExampleClass()
 result = is_same_class(obj, ExampleClass)
 print(result)  # Output: True
-
-
-
