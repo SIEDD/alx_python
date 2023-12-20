be212e4f908dfa94d154166857286a8dce5d3fd5
siedd <saidmwacharo8@gmail.com>
2023-12-20 19:13:26 +0300

    geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,1 +1,14 @@
+class BaseGeometry:
+    """A base class representing basic geometry.
+
+    Attributes:
+        None
+
+    Methods:
+        area(): Raises an Exception indicating that area() is not implemented.
+    """
+
+    def area(self):
+        """Raise an exception indicating that area() is not implemented."""
+        raise Exception("area() is not implemented")
 
