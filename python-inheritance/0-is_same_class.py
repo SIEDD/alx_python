b394fb08c25e8dac395883f850da1c02c1a92a14
siedd <saidmwacharo8@gmail.com>
2023-12-20 17:43:44 +0300

    inherit

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,1 +1,15 @@
+def is_same_class(obj, a_class):
+    """Check if obj is an instance of a_class."""
+    return type(obj) is a_class
+
+# Example usage:
+class ExampleClass:
+    pass
+
+obj = ExampleClass()
+result = is_same_class(obj, ExampleClass)
+print(result)  # Output: True
+
+result = is_same_class(obj, int)
+print(result)  # Output: False
 
