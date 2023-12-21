b1ebd2ff463690e9c36089545258e0d0c368d6cc
siedd <saidmwacharo8@gmail.com>
2023-12-21 15:54:13 +0300

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,23 @@
-"""class representing a square"""
+# """class representing a square"""
+# class Square:
+#     """A class representing a square.
+
+#     Attributes:
+#         __size (int): The size of the square.
+
+#     Methods:
+#         size() -> int: Getter method to retrieve the size of the square.
+#     """
+
+#     def __init__(self, size):
+#         """Initialize a square with a given size."""
+#         self.__size = size
+    
+#     @property
+#     def size(self):
+#         """Get the size of the square."""
+#         return self.__size
+"""class representing the square"""
 class Square:
     """A class representing a square.
 
@@ -12,9 +31,14 @@ class Square:
     def __init__(self, size):
         """Initialize a square with a given size."""
         self.__size = size
-    
+
     @property
     def size(self):
         """Get the size of the square."""
         return self.__size
+
+# Example usage
+square = Square(3)
+print(square.__class__)
+print(square.__dict__)
 
