221356d934b4479684e118674012a60ea68c7253
siedd <saidmwacharo8@gmail.com>
2023-12-21 23:44:28 +0300

    OK

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -71,4 +71,4 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError(f"{attribute} must be an integer")
         elif value < 0:
-            raise ValueError(f"{attribute} must be > 0")
+            raise ValueError(f"{attribute} must be >= 0")
