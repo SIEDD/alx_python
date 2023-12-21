a0072d91f616b0fcd6ec82b6226afc077806a252
siedd <saidmwacharo8@gmail.com>
2023-12-21 17:15:51 +0300

    inherit

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -21,3 +21,9 @@ class BaseGeometry:
     def area(self):
         """Raise an exception indicating that area() is not implemented."""
         raise Exception("area() is not implemented.")
+    def __dir__(self):
+        return sorted(['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', 
+                       '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', 
+                       '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', 
+                       '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'area'
+                       ])
