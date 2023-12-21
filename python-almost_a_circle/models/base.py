3293bb178ab7ee16431100ffa8753ccf33d4e76c
siedd <saidmwacharo8@gmail.com>
2023-12-21 22:09:54 +0300

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,1 +1,22 @@
+# models/base.py
+
+class Base:
+    """Base class for other classes."""
+
+    # private class attribute
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Constructor for the Base class.
+
+        Args:
+            id (int, optional): If provided, assign it to the public instance attribute id.
+                               Otherwise, increment __nb_objects and assign the new value to id.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
