ee0cd3327b33d2218f9002345bbb009b8fe1ca10
siedd <saidmwacharo8@gmail.com>
2023-12-22 01:01:02 +0300

    getters and setters

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -5,6 +5,14 @@ class Square(Rectangle):
     """super method"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value    
     """str method"""
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
