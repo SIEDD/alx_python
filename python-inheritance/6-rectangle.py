f17ef772db2b89073da261c758062d6b3f66d468
siedd <saidmwacharo8@gmail.com>
2023-12-21 18:07:08 +0300

    geometry

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,1 +1,7 @@
-
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("width", width)
