9ee757265a1d768882adf108cd5452725cd962db
siedd <saidmwacharo8@gmail.com>
2023-12-22 00:54:10 +0300

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,1 +1,11 @@
+"""importing from rectangle class"""
+from models.rectangle import Rectangle
+"""the square class which has rectangle attributes"""
+class Square(Rectangle):
+    """super method"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+    """str method"""
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
 
