2c896b6e288faa8134b1d19bf34dae7e65f5ea3e
siedd <saidmwacharo8@gmail.com>
2023-12-21 17:10:42 +0300

    inherit

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,7 +1,6 @@
 class BaseGeometry:
     """<3-base_geometry.BaseGeometry object at"""
-    def __init__(self):
-        """Initialize a new instance of BaseGeometry."""
-        pass
-        help(BaseGeometry)
-print(BaseGeometry.__doc__)
+    # def __init__(self):
+pass
+        # help(BaseGeometry)
+# print(BaseGeometry.__doc__)
