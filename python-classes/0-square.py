59669ac561dc190743611c731b8f77a83655640c
siedd <saidmwacharo8@gmail.com>
2023-12-21 16:16:16 +0300

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,24 +1,3 @@
-
-# """class representing the square"""
-# class Square:
-#     """A class representing a square.
-
-#     Attributes:
-#         __size (int): The size of the square.
-
-#     Methods:
-#         size() -> int: Getter method to retrieve the size of the square.
-#     """
-
-#     def __init__(self, size):
-#         """Initialize a square with a given size."""
-#         self.__size = size
-
-#     @property
-#     def size(self):
-#         """Get the size of the square."""
-#         return self.__size
-
 """class for square"""
 class Square:
     """A class representing a square.
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,24 @@
+"""
+This module defines the Square class.
+
+The Square class represents a square with a specified size.
+
+Attributes:
+    __size (int): The size of the square.
+"""
+
 class Square:
     def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
