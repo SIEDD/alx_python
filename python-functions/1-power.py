885f372deab2dc3bcc4c5252a259ff84c357be8f
siedd <saidmwacharo8@gmail.com>
2023-12-26 12:57:35 +0300

    power

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,15 +1,7 @@
-# def pow(a, b):
-#     result = 1
-#     for _ in range(b):
-#         result *= a
-#     return result
-
-
 def pow(a, b):
     result = 1
     for _ in range(b):
         result *= a
     return result
 
-result = pow(0.01, 2)
-print(result)
+
