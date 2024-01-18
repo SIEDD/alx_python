f18f1c26dbe024b318ef84d83b551bdd9ede68d2
siedd <saidmwacharo8@gmail.com>
2024-01-18 23:18:05 +0300

    filter

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,37 @@
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    if len(argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(argv[0]))
+        exit(1)
+
+    username, password, database = argv[1], argv[2], argv[3]
+
+    # Connecting to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    query = """SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id """
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    for i, state in enumerate(states):
+        print(state[0], end='')
+        if i < len(states)-1:
+            print(', ', end='')
+    print('')
+
+    cursor.close()
+    db.close()
+
 
