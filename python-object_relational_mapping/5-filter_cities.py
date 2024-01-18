2520350d37ee8db0f9afc07c687d5d23c0a2f58f
siedd <saidmwacharo8@gmail.com>
2024-01-18 23:22:52 +0300

    filter

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,8 +1,43 @@
+# import MySQLdb
+# from sys import argv
+
+# if __name__ == "__main__":
+
+#     if len(argv) != 4:
+#         print("Usage: {} <username> <password> <database>".format(argv[0]))
+#         exit(1)
+
+#     username, password, database = argv[1], argv[2], argv[3]
+
+#     # Connecting to the MySQL server
+#     db = MySQLdb.connect(
+#         host="localhost",
+#         port=3306,
+#         user=username,
+#         passwd=password,
+#         db=database
+#     )
+
+#     cursor = db.cursor()
+
+#     query = """SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id """
+#     cursor.execute(query)
+
+#     rows = cursor.fetchall()
+
+#     for i, state in enumerate(states):
+#         print(state[0], end='')
+#         if i < len(states)-1:
+#             print(', ', end='')
+#     print('')
+
+#     cursor.close()
+#     db.close()
+
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-
     if len(argv) != 4:
         print("Usage: {} <username> <password> <database>".format(argv[0]))
         exit(1)
@@ -20,18 +55,18 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    query = """SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id """
+    query = """SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id"""
     cursor.execute(query)
 
     rows = cursor.fetchall()
 
-    for i, state in enumerate(states):
-        print(state[0], end='')
-        if i < len(states)-1:
+    for i, row in enumerate(rows):
+        print(f"{row[0]}: {row[1]}, {row[2]}", end='')
+        if i < len(rows) - 1:
             print(', ', end='')
+
     print('')
 
     cursor.close()
     db.close()
-
 
