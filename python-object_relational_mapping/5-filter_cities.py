588789c92ae54a8cba85d97d2e36f9a04b5fa1a0
siedd <saidmwacharo8@gmail.com>
2024-01-18 23:47:36 +0300

    filter

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -2,9 +2,9 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    mysql_username = argv[1],
-    mysql_password = argv[2],
-    mysql_database = argv[3],
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    mysql_database = argv[3]
     state_name = argv[4]
     connect = MySQLdb.connect(host="localhost",
         port=3306,
