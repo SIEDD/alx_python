96b9e112234b94af16e80a234711213518ce946c
siedd <saidmwacharo8@gmail.com>
2024-01-18 23:44:49 +0300

    filter

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,72 +1,23 @@
-# import MySQLdb
-# from sys import argv
-
-# if __name__ == "__main__":
-
-#     if len(argv) != 4:
-#         print("Usage: {} <username> <password> <database>".format(argv[0]))
-#         exit(1)
-
-#     username, password, database = argv[1], argv[2], argv[3]
-
-#     # Connecting to the MySQL server
-#     db = MySQLdb.connect(
-#         host="localhost",
-#         port=3306,
-#         user=username,
-#         passwd=password,
-#         db=database
-#     )
-
-#     cursor = db.cursor()
-
-#     query = """SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id """
-#     cursor.execute(query)
-
-#     rows = cursor.fetchall()
-
-#     for i, state in enumerate(states):
-#         print(state[0], end='')
-#         if i < len(states)-1:
-#             print(', ', end='')
-#     print('')
-
-#     cursor.close()
-#     db.close()
-
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    if len(argv) != 4:
-        print("Page, Phoenix")
-        exit(1)
-
-    username, password, database = argv[1], argv[2], argv[3]
-
-    # Connecting to the MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
+    mysql_username = argv[1],
+    mysql_password = argv[2],
+    mysql_database = argv[3],
+    state_name = argv[4]
+    connect = MySQLdb.connect(host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
-    cursor = db.cursor()
-
-    query = """SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id"""
-    cursor.execute(query)
-
+        user=mysql_username,
+        passwd=mysql_password,
+        db=mysql_database)
+    cursor = connect.cursor()
+    query = """SELECT cities.name FROM cities INNER JOIN states ON cities.states_id = states.id WHERE states.name = %s ORDER BY cities.id ASC"""
+    cursor.execute(query,(state_name,))
     rows = cursor.fetchall()
-
-    for i, row in enumerate(rows):
-        print(f"{row[0]}: {row[1]}, {row[2]}", end='')
-        if i < len(rows) - 1:
-            print(', ', end='')
-
-    print('')
-
+    cities = [row[0]for row in rows]
+    print(", ".join(cities))
     cursor.close()
-    db.close()
+    connect.close()
+
 
