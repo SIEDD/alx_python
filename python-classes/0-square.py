85ffaf4b47a674cf016756fd649d6bd9e0a49485
siedd <saidmwacharo8@gmail.com>
2024-02-13 12:43:57 +0300

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,27 +1,11 @@
-"""class for square"""
+"""Defines a class Square"""
 class Square:
-    """A class representing a square.
-
-    Attributes:
-        __size (int): The size of the square.
-
-    Methods:
-        size() -> int: Getter method to retrieve the size of the square.
+    """Class that defines properties of square.
     """
-
     def __init__(self, size):
-        """Initialize a square with a given size."""
+        """Initialize a Square object with a specified size.
+
+        Args:
+            size (int): The size of the square. Defaults to 3.
+        """
         self.__size = size
-
-    @property
-    def size(self):
-        """Get the size of the square."""
-        return self.__size
-
-# square = Square(3)
-
-# try:
-#     print(square.size)
-# except AttributeError:
-#     raise AttributeError("'Square' object has no attribute 'size'")
-
