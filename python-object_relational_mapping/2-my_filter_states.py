3e9adf16c57d227f71184a7b2dfd70c542dbc964
siedd <saidmwacharo8@gmail.com>
2024-02-13 13:04:39 +0300

    states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,31 +1,24 @@
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
-    
-    if len(argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(argv[0]))
-        exit(1)
+if __name__ == '__main__':
+    db_config = {
+        'host':     'localhost',
+        'user':     argv[1],
+        'passwd':   argv[2],
+        'db':       argv[3],
+        'port':     3306
+    }
 
-    username, password, database, state_name = argv[1], argv[2], argv[3], argv[4]
+    conn = MySQLdb.connect(**db_config)
+    cursor = conn.cursor()
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
-    cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    query = """ SELECT * FROM states WHERE name like BINARY '{}'
+                ORDER BY id ASC
+            """.format(argv[4])
     cursor.execute(query)
-
-    rows = cursor.fetchall()
-
-    for row in rows:
-        print(row)
-
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
     cursor.close()
-    db.close()
-
+    conn.close()
