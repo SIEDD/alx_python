fcecc240309c3d5632ab357059d86a1ddafe3170
siedd <saidmwacharo8@gmail.com>
2024-02-13 12:13:02 +0300

    arguments

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,21 +1,7 @@
-import sys
 if __name__ == "__main__":
-    def print_arguments():
-        num_arguments = len(sys.argv) - 1
-        print(num_arguments, end=" ")
-        if num_arguments == 1:
-            print("argument:",end="")
-        elif num_arguments > 1:
-            print("arguments:",end="")
-        else:
-            print(".", end="")
-        return
 
-    for i, arg in enumerate(sys.argv[1:],start=1):
-        print("\n{}: {}".format(i, arg))
+    from args_1 import print_arguments
 
-
-
-
-
-
+    result = print_arguments()
+    if result:
+        print(result)
