4b94118a775cce45a49133bb8193e14a689f9709
siedd <saidmwacharo8@gmail.com>
2024-02-29 12:02:02 +0300

    export

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -3,23 +3,21 @@ import requests
 import sys
 
 def get_employee_info(employee_id):
+    # Fetch employee details and TODO list
     employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     response = requests.get(employee_url)
     employee_data = response.json()
-    user_id = employee_data['id']
-    username = employee_data['username']
-
     todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
     response = requests.get(todos_url)
     todos_data = response.json()
 
-    filename = f"{user_id}.csv"
+    # Write to CSV file
+    filename = f"{employee_id}.csv"
     with open(filename, 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        csv_writer.writerow(["USER_ID", "TASK_ID", "TASK_TITLE", "TASK_COMPLETED_STATUS"])
         for task in todos_data:
-            task_completed_status = "completed" if task['completed'] else "not completed"
-            csv_writer.writerow([user_id, username, task_completed_status, task['title']])
+            csv_writer.writerow([employee_id, task['id'], task['title'], task['completed']])
 
     print(f"Data exported to {filename}")
 
@@ -36,5 +34,3 @@ if __name__ == "__main__":
         sys.exit(1)
 
     get_employee_info(employee_id)
-
-
