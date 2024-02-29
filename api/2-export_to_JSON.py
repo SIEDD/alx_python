8f624106d1eef97c5aef540a021907ca5e71e7d4
siedd <saidmwacharo8@gmail.com>
2024-02-29 23:24:42 +0300

    doc

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,16 +1,17 @@
+"""importing libraries"""
 import json
 import requests
 import sys
-
+"""employee informatiom"""
 def get_employee_info(employee_id):
     employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
     employee_response = requests.get(employee_url)
     employee_data = employee_response.json()
-
+    """url, response and data"""
     todos_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
     todos_response = requests.get(todos_url)
     todos_data = todos_response.json()
-
+    """file path"""
     json_file_path = f"{employee_id}.json"
     json_data = {
         f"{employee_id}": [
