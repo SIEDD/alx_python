95fada7c4509bbd5b4bd35f8d39c7de7807041e6
siedd <saidmwacharo8@gmail.com>
2024-02-29 11:50:07 +0300

    Employee details

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,19 +2,23 @@ import requests
 import sys
 
 def get_employee_info(employee_id):
+    # Fetch employee details
     employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     response = requests.get(employee_url)
     employee_data = response.json()
     employee_name = employee_data['name']
 
+    # Fetch employee's TODO list
     todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
     response = requests.get(todos_url)
     todos_data = response.json()
 
+    # Count completed tasks
     completed_tasks = [task for task in todos_data if task['completed']]
     num_completed_tasks = len(completed_tasks)
     total_tasks = len(todos_data)
 
+    # Print employee TODO list progress
     print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t{task['title']}")
@@ -32,4 +36,3 @@ if __name__ == "__main__":
         sys.exit(1)
 
     get_employee_info(employee_id)
-
