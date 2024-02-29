868c46448e9fda84d0f482f6c248775fc8bbf5df
siedd <saidmwacharo8@gmail.com>
2024-02-29 22:46:55 +0300

    api

--- python-inheritance/6-rectangle.py
+++
@@ -1,7 +0,0 @@
-BaseGeometry = __import__('5-base_geometry').BaseGeometry
-class Rectangle(BaseGeometry):
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
